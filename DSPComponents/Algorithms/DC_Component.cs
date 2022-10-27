041b13804bef73314756444ab2015757b8e53aac
yousefimam <63289772+yousefimam@users.noreply.github.com>
2022-10-27 17:24:13 +0200

    DC_Component added

---
+++ DSPComponents/Algorithms/DC_Component.cs
@@ -0,0 +1,26 @@
+﻿using DSPAlgorithms.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSPAlgorithms.Algorithms
+{
+    public class DC_Component: Algorithm
+    {
+        public Signal InputSignal { get; set; }
+        public Signal OutputSignal { get; set; }
+
+        public override void Run()
+        {
+            List<float> Output = new List<float>();
+            float MEAN = InputSignal.Samples.Average();
+            for (int i = 0; i < InputSignal.Samples.Count; i++)
+            {
+                Output.Add(InputSignal.Samples[i] - MEAN);   
+            }
+            OutputSignal = new Signal(Output, false);
+        }
+    }
+}
