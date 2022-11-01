c05e20e7e64d65813d513d5ce5c4ca477f9bde1a
yousefimam <63289772+yousefimam@users.noreply.github.com>
2022-11-01 20:25:19 +0200

    IDFT Added

--- DSPComponents/Algorithms/InverseDiscreteFourierTransform.cs
+++ DSPComponents/Algorithms/InverseDiscreteFourierTransform.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 using DSPAlgorithms.DataStructures;
 
 namespace DSPAlgorithms.Algorithms
@@ -14,7 +15,32 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            float Real, Imaginary, Power , Real1, Imaginary1;
+            Complex X, Y;
+            List<float> Amplitudes = InputFreqDomainSignal.FrequenciesAmplitudes;
+            List<float> PhaseShifts = InputFreqDomainSignal.FrequenciesPhaseShifts;
+            List<float> Samples = new List<float> ();
+            for (int k = 0; k < Amplitudes.Count; k++)
+            {
+                Complex Result = new Complex(0.0 , 0.0);
+                for (int n = 0; n < Amplitudes.Count; n++) //2*PI*J*k*n/N
+                {
+                    Power = ((((float)Math.PI) * 2 * n * k) / Amplitudes.Count());
+                    Real1 = (float)Math.Cos(PhaseShifts[n]) * Amplitudes[n];
+                    Imaginary1 = (float)Math.Sin(PhaseShifts[n]) * Amplitudes[n];
+                    X = new Complex(Real1, Imaginary1);
+                    Real = (float)Math.Cos(Power);
+                    Imaginary = (float)Math.Sin(Power);
+                    Y = new Complex(Real, Imaginary);
+                    Complex mult = Complex.Multiply(X, Y);
+                    Result = Complex.Add(Result,mult);
+                }
+                float Sample = (float)  Math.Round(Result.Real)/Amplitudes.Count;
+                Samples.Add(Sample);
+            }
+            OutputTimeDomainSignal = new Signal(Samples, false);
+            
+            
         }
     }
 }
