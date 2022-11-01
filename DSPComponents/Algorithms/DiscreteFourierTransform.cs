f4d379103f99772ba4191abcafd57b9f513c5003
waseem2023 <96105710+waseem2023@users.noreply.github.com>
2022-11-01 15:05:54 +0200

    DFT

--- DSPComponents/Algorithms/DiscreteFourierTransform.cs
+++ DSPComponents/Algorithms/DiscreteFourierTransform.cs
@@ -13,9 +13,33 @@ namespace DSPAlgorithms.Algorithms
         public float InputSamplingFrequency { get; set; }
         public Signal OutputFreqDomainSignal { get; set; }
 
+
         public override void Run()
         {
-            throw new NotImplementedException();
+            float Real, Imaginary,Power;
+            List<float> Amplitudes = new List<float>();
+            List<float> PhaseShifts = new List<float>();
+
+            List<float> Signals = InputTimeDomainSignal.Samples;
+            for (int k = 0; k < Signals.Count; k++) 
+            {
+                Real = 0;
+                Imaginary = 0;
+                for (int n = 0; n < Signals.Count; n++) //2*PI*J*k*n/N
+                {
+                    Power = ((((float)Math.PI) * 2 * n * k) / Signals.Count());
+                    Real += (float)Math.Cos(Power) * Signals[n];
+                    Imaginary += (float)Math.Sin(Power) *( - Signals[n]);
+                }
+                var Amplitude = (float)Math.Sqrt(Real * Real + Imaginary * Imaginary);
+                var PhaseShift = Math.Atan2(Imaginary, Real); //theta=imaginary/real
+                Amplitudes.Add(Amplitude);
+                PhaseShifts.Add((float)PhaseShift);
+            }
+
+            OutputFreqDomainSignal = new Signal(Signals, false);
+            OutputFreqDomainSignal.FrequenciesAmplitudes = Amplitudes;
+            OutputFreqDomainSignal.FrequenciesPhaseShifts = PhaseShifts;
         }
     }
 }
