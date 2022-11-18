bdf5e6890100a50817baa83568bb1089b3e53e85
yousefimam <63289772+yousefimam@users.noreply.github.com>
2022-11-18 21:54:50 +0200

    Fast convolution added

--- DSPComponents/Algorithms/FastConvolution.cs
+++ DSPComponents/Algorithms/FastConvolution.cs
@@ -18,7 +18,61 @@ namespace DSPAlgorithms.Algorithms
         /// </summary>
         public override void Run()
         {
-            throw new NotImplementedException();
+            //List<List<float>> matrix = new List<List<float>>();
+            float[,] matrix = new float[InputSignal2.Samples.Count, InputSignal1.Samples.Count];
+            List<float> Output = new List<float>();
+            List<int> Output_Indeices = new List<int>();
+            for (int i = 0; i < InputSignal2.Samples.Count; i++)
+            {
+                for (int j = 0; j < InputSignal1.Samples.Count; j++)
+                {
+                    matrix[i, j] = InputSignal2.Samples[i] * InputSignal1.Samples[j];
+                }
+            }
+
+            int Width = InputSignal1.Samples.Count;
+            int Hieght = InputSignal2.Samples.Count;
+            for (int col = 0; col < Width; col++)
+            {
+                int startcol = col, startrow = 0;
+                float sum = 0;
+
+                while (startcol >= 0 && startrow < Hieght)
+                {
+                    sum += matrix[startrow, startcol];
+                    startcol--;
+                    startrow++;
+                }
+                Output.Add(sum);
+            }
+
+            // For each row start column is N-1
+            for (int row = 1; row < Hieght; row++)
+            {
+                int startrow = row, startcol = Width - 1;
+                float sum = 0;
+                while (startrow < Hieght && startcol >= 0)
+                {
+                    sum += matrix[startrow, startcol];
+                    startcol--;
+                    startrow++;
+                }
+                Output.Add(sum);
+            }
+            int index_of_zero_1stlist = 0, index_of_zero_2ndlist = 0, origin = 0;
+            if (InputSignal1.SamplesIndices.Count > 0)
+            {
+                index_of_zero_1stlist = InputSignal1.SamplesIndices.IndexOf(0);
+                index_of_zero_2ndlist = InputSignal2.SamplesIndices.IndexOf(0);
+                origin = index_of_zero_1stlist + index_of_zero_2ndlist;
+            }
+            for (int i = 0; i < Output.Count; i++)
+            {
+                Output_Indeices.Add(i - origin);
+            }
+
+            OutputConvolvedSignal = new Signal(Output, Output_Indeices, false);
+
         }
     }
 }
